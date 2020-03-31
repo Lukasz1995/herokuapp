f1d1a69145294a47fd87a0ee5748dafe295a370a
Luke <lukas142@wp.pl>
2020-04-01 00:22:43 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const shell = require('shelljs');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.get('/', function(req, res){
+
+    shell.exec(`java -jar helloword.jar /usr/local/bin/wn`);
+    let jsonData = JSON.parse(fs.readFileSync('./dictionary.json', 'utf8'));
+
+    res.send(jsonData)
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
