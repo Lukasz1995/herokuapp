9e7c53371880900983f6fcda839062301f5145a8
Luke <lukas142@wp.pl>
2020-04-01 00:48:45 +0200

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const shell = require('shelljs');
 const fs = require('fs');
 const bodyParser = require('body-parser');
